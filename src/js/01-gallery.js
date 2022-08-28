b0b4d05b7e46b8d4dd56c7b2eb1527e4d6920977
SPoluliakh <S_Poluliakh@ukr.net>
2022-08-28 22:25:16 +0300

    added simplelightbox by using npm

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
+
+const gallaryListEl = document.querySelector('.gallery');
+
+const galleryItemsEl = createGalery(galleryItems);
+
+gallaryListEl.innerHTML = galleryItemsEl;
+
+function createGalery(arr) {
+  return arr
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" title= "${description}"/>
+</a>`;
+    })
+    .join('');
+}
+
+new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+});
