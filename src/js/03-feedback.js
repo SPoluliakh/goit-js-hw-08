b6a5f6699a99a9d55d10bbf9d3d3bddede470ac9
SPoluliakh <S_Poluliakh@ukr.net>
2022-08-30 17:09:00 +0300

    3 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import { throttle } from 'lodash';
+const formEl = document.querySelector('.feedback-form');
+const emailEl = document.querySelector('input[type="email"]');
+const textareaEL = document.querySelector('textarea[name="message"]');
 
+formEl.addEventListener('input', throttle(onFormInputField, 500));
+formEl.addEventListener('submit', onFormSubmit);
+const formDate = {};
+saveFormDate();
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  const formDateValue = localStorage.getItem('feedbackFormState');
+  const savedFormDateValue = JSON.parse(formDateValue);
+  console.log(savedFormDateValue);
+  localStorage.removeItem('feedbackFormState');
+  e.target.reset();
+}
+
+function onFormInputField(e) {
+  formDate[e.target.name] = e.target.value;
+
+  localStorage.setItem('feedbackFormState', JSON.stringify(formDate));
+}
+
+function saveFormDate() {
+  const currentDate = localStorage.getItem('feedbackFormState');
+  if (currentDate) {
+    const formDateValue = localStorage.getItem('feedbackFormState');
+    const savedFormDateValue = JSON.parse(formDateValue);
+
+    emailEl.value = savedFormDateValue.email;
+    textareaEL.value = savedFormDateValue.message;
+  }
+}
