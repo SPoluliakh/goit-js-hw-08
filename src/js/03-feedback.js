0afc11c29cfdd19079f1e382c687bca3b3d37b9d
SPoluliakh <S_Poluliakh@ukr.net>
2022-09-14 18:54:07 +0300

    updated

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -13,26 +13,26 @@ saveFormDate();
 
 function onFormSubmit(e) {
   e.preventDefault();
-  const formDateValue = localStorage.getItem('feedbackFormState');
-  const savedFormDateValue = JSON.parse(formDateValue);
-  console.log(savedFormDateValue);
+
   removeItemFromLocalStor();
   e.target.reset();
 }
 
 function onFormInput(e) {
   formDate[e.target.name] = e.target.value;
+  console.log(formDate);
 
   localStorage.setItem('feedbackFormState', JSON.stringify(formDate));
 }
 
 function saveFormDate() {
-  const currentDate = localStorage.getItem('feedbackFormState');
   const formDateValue = localStorage.getItem('feedbackFormState');
   const savedFormDateValue = JSON.parse(formDateValue);
-  if (currentDate) {
+  if (formDateValue) {
     refs.emailEl.value = savedFormDateValue.email;
     refs.textareaEL.value = savedFormDateValue.message;
+    formDate.email = savedFormDateValue.email;
+    formDate.message = savedFormDateValue.message;
   }
 }
 
